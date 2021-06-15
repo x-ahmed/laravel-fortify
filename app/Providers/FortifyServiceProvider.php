8950235300fda4aad711b6edf24c24a3d76e96d7
x-ahmed <appsxapp@gmail.com>
2021-06-15 23:06:39 +0200

    created registeration feature

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -43,5 +43,8 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('two-factor', function (Request $request) {
             return Limit::perMinute(5)->by($request->session()->get('login.id'));
         });
+
+        // Authentication Views
+        Fortify::registerView(fn()=> view('auth.register'));
     }
 }
